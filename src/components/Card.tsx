3964cea46501faa983482454d5e652ab8c4a68f6
aliwert <154356044+aliwert@users.noreply.github.com>
2024-05-26 23:27:49 +0300

    colors have been set for the card component for dark and light themes.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,12 +9,10 @@ interface ICard {
 
 const Card: React.FC<ICard> = ({ item, text, handleFunc }) => {
   return (
-    <div className="w-10/12 sm:w-6/12 md:w-4/12 lg:w-3/12 flex flex-col justify-between bg-white rounded-lg">
+    <div className="w-10/12 sm:w-6/12 md:w-4/12 lg:w-3/12 flex flex-col justify-between text-lg bg-white rounded-lg dark:bg-gray-400 dark:text-white">
       <div className="p-4 ">
-        <h1 className="text-gray-900 uppercase text-xl line-clamp-1 hover:line-clamp-none">
-          {item.title}
-        </h1>
-        <p className="text-sm mt-1 text-gray-500 line-clamp-1 hover:line-clamp-none">
+        <h1 className="text-gray-900 uppercase text-xl">{item.title}</h1>
+        <p className="text-sm mt-1 line-clamp-1 hover:line-clamp-none">
           {item.description}
         </p>
       </div>
@@ -25,8 +23,12 @@ const Card: React.FC<ICard> = ({ item, text, handleFunc }) => {
       />
       <div className="flex justify-between items-center bg-gray-700 p-3">
         <h2 className="text-gray-100 font-bold ">${item.price}</h2>
+        <p className="text-xl text-gray-200 capitalize border-indigo-600 bg-inherit p-1 border-collapse rounded-lg ">
+          {" "}
+          {item.category}{" "}
+        </p>
         <button
-          className="bg-gray-300 p-1 rounded-lg animate-pulse hover:animate-none"
+          className="bg-gray-300 p-1 rounded-lg "
           onClick={() => handleFunc(item)}
         >
           {text}
