500a6ea021d23365118734013745f58ae5f86c30
aliwert <154356044+aliwert@users.noreply.github.com>
2024-05-26 21:22:45 +0300

    navbar created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center px-3 h-[60px] text-white bg-gray-600">
+      <h3>AliWert Store</h3>
+      <div>
+        <Link to="/" className="mr-2">
+          Home
+        </Link>
+        <Link to="/favorite">Favorites</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
