2f9ea3ab08d1d3b385f485198b257824e9a0cd1c
aliwert <154356044+aliwert@users.noreply.github.com>
2024-05-26 21:20:39 +0300

    app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { Flip, ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div className="bg-gray-400 min-h-screen">
+    <div className="bg-gray-600 min-h-screen">
       <Provider store={store}>
         <BrowserRouter>
           <Navbar />
